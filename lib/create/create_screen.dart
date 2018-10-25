4ac56c3009cbe1ac9397aa805490cff8204a6fa5
david <ariedov@gmail.com>
2018-10-25 07:57:15 +0300

    adds basic screen transition

--- lib/create/create_screen.dart
+++ lib/create/create_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:flutter_todo/data_provider.dart';
 import 'package:flutter_todo/model.dart';
 
 class CreateScreen extends StatefulWidget {
   final Category category;
-  const CreateScreen({Key key, this.category}) : super(key: key);
+  final VoidCallback onClose;
+
+  const CreateScreen({Key key, this.category, this.onClose}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _CreateScreenState();
@@ -22,59 +22,37 @@ class _CreateScreenState extends State<CreateScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          leading: IconButton(
-            icon: Icon(Icons.close, color: Colors.black87),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          title: Text(
-            "New Task",
-            style: TextStyle(color: Colors.black87),
-          ),
-        ),
-        body: Column(
+    return Column(
+      children: <Widget>[
+        Row(
           children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(80.0),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      controller: _taskController,
-                      autofocus: true,
-                      style: Theme.of(context).textTheme.title,
-                      decoration: InputDecoration(
-                          labelText: "What task are you planning to perform?",
-                          labelStyle: Theme.of(context).textTheme.subhead),
-                    ),
-                  ],
-                ),
-              ),
+            IconButton(
+              icon: Icon(Icons.close, color: Colors.black87),
+              onPressed: widget.onClose,
             ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(color: Colors.purple),
-              child: StoreConnector<CategoryState, ValueChanged<String>>(
-                converter: (store) =>
-                    (name) => store.dispatch(AddTask(widget.category, name)),
-                builder: (context, callback) => IconButton(
-                    icon: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      String text = _taskController.text;
-                      if (text != null && text.isNotEmpty) {
-                        callback(text);
-                      }
-                      Navigator.of(context).pop();
-                    }),
-              ),
+            Text(
+              "New Task",
+              style: TextStyle(color: Colors.black87),
             ),
           ],
-        ));
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(80.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  controller: _taskController,
+                  style: Theme.of(context).textTheme.title,
+                  decoration: InputDecoration(
+                      labelText: "What task are you planning to perform?",
+                      labelStyle: Theme.of(context).textTheme.subhead),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/detail/detail_screen.dart
+++ lib/detail/detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_todo/category_icon.dart';
 import 'package:flutter_todo/category_info.dart';
+import 'package:flutter_todo/create/create_screen.dart';
 import 'package:flutter_todo/data_provider.dart';
 import 'package:flutter_todo/detail/todo_item.dart';
 import 'package:flutter_todo/model.dart';
@@ -28,6 +29,12 @@ class _DetailScreenState extends State<DetailScreen>
   double buttonRadius = 48.0;
   double buttonPosition = 16.0;
 
+  Tween<double> listOpacityTween;
+  Tween<double> createOpacityTween;
+
+  double listOpacity;
+  double createOpacity;
+
   @override
   void initState() {
     _transitionAnimation =
@@ -37,10 +44,15 @@ class _DetailScreenState extends State<DetailScreen>
                 buttonPosition =
                     buttonPositionTween.evaluate(_transitionAnimation);
                 buttonRadius = buttonRadiusTween.evaluate(_transitionAnimation);
+
+                listOpacity = listOpacityTween.evaluate(_transitionAnimation);
+                createOpacity = createOpacityTween.evaluate(_transitionAnimation);
               }))
           ..addStatusListener((status) {
             if (status == AnimationStatus.completed) {
-              creatingTask = !creatingTask;
+              setState(() {
+                creatingTask = !creatingTask;
+              });
             }
           });
 
@@ -50,54 +62,11 @@ class _DetailScreenState extends State<DetailScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: true,
       body: SafeArea(
         child: Stack(
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(80.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Hero(
-                    tag: "categoryIcon${widget.category.title}",
-                    child: CategoryIcon(
-                      icon: widget.category.icon,
-                      color: widget.category.color,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 32.0,
-                  ),
-                  Hero(
-                      tag: "categoryInfo${widget.category.title}",
-                      child: CategoryInfo(category: widget.category)),
-                  SizedBox(
-                    height: 24.0,
-                  ),
-                  Expanded(
-                    child: AnimatedOpacity(
-                      opacity: 1.0,
-                      child: StoreConnector<CategoryState, List<Task>>(
-                          converter: (store) => store.state.categories
-                              .firstWhere((category) =>
-                                  category.id == widget.category.id)
-                              .tasks,
-                          builder: (context, tasks) {
-                            return ListView.builder(
-                              itemBuilder: (BuildContext context, int index) {
-                                final task = tasks[index];
-                                return TodoItem(
-                                    category: widget.category, task: task);
-                              },
-                              itemCount: tasks.length,
-                            );
-                          }),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  )
-                ],
-              ),
-            ),
+            _buildContent(),
             Positioned(
               bottom: buttonPosition,
               right: buttonPosition,
@@ -114,18 +83,7 @@ class _DetailScreenState extends State<DetailScreen>
                     color: Colors.white,
                   ),
                   onPressed: () {
-                    final width = MediaQuery.of(context).size.width;
-                    buttonSizeTween = Tween(begin: 48.0, end: width);
-                    buttonPositionTween = Tween(begin: 16.0, end: 0.0);
-                    buttonRadiusTween = Tween(begin: 48.0, end: 0.0);
-
-                    if (creatingTask) {
-                      buttonSizeTween = ReverseTween(buttonSizeTween);
-                      buttonPositionTween = ReverseTween(buttonPositionTween);
-                      buttonRadiusTween = ReverseTween(buttonRadiusTween);
-                    }
-
-                    _transitionAnimation.forward(from: 0.0);
+                    _startTransitionAnimation();
                   },
                 ),
               ),
@@ -133,6 +91,103 @@ class _DetailScreenState extends State<DetailScreen>
           ],
         ),
       ),
+    );
+  }
+
+  _startTransitionAnimation() {
+    final width = MediaQuery.of(context).size.width;
+    buttonSizeTween = Tween(begin: 48.0, end: width);
+    buttonPositionTween = Tween(begin: 16.0, end: 0.0);
+    buttonRadiusTween = Tween(begin: 48.0, end: 0.0);
+
+    listOpacityTween = Tween(begin: 1.0, end: 0.0);
+    createOpacityTween = Tween(begin: 0.0, end: 1.0);
+
+    if (creatingTask) {
+      buttonSizeTween = ReverseTween(buttonSizeTween);
+      buttonPositionTween = ReverseTween(buttonPositionTween);
+      buttonRadiusTween = ReverseTween(buttonRadiusTween);
+
+      listOpacityTween = ReverseTween(listOpacityTween);
+      createOpacityTween = ReverseTween(createOpacityTween);
+    }
+
+    _transitionAnimation.forward(from: 0.0);
+  }
+
+  _buildContent() {
+    if (_transitionAnimation.isAnimating) {
+      return Stack(
+        children: <Widget>[
+          Opacity(
+            opacity: createOpacity,
+            child: _buildCreate(),
+          ),
+          Opacity(
+            opacity: listOpacity,
+            child: _buildTaskList(),
+          ),
+        ],
+      );
+    }
+
+    if (creatingTask) {
+      return _buildCreate();
+    }
+    return _buildTaskList();
+  }
+
+  _buildTaskList() {
+    return Container(
+      padding: const EdgeInsets.all(80.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Hero(
+            tag: "categoryIcon${widget.category.title}",
+            child: CategoryIcon(
+              icon: widget.category.icon,
+              color: widget.category.color,
+            ),
+          ),
+          SizedBox(
+            height: 32.0,
+          ),
+          Hero(
+              tag: "categoryInfo${widget.category.title}",
+              child: CategoryInfo(category: widget.category)),
+          SizedBox(
+            height: 24.0,
+          ),
+          Expanded(
+            child: AnimatedOpacity(
+              opacity: 1.0,
+              child: StoreConnector<CategoryState, List<Task>>(
+                  converter: (store) => store.state.categories
+                      .firstWhere(
+                          (category) => category.id == widget.category.id)
+                      .tasks,
+                  builder: (context, tasks) {
+                    return ListView.builder(
+                      itemBuilder: (BuildContext context, int index) {
+                        final task = tasks[index];
+                        return TodoItem(category: widget.category, task: task);
+                      },
+                      itemCount: tasks.length,
+                    );
+                  }),
+              duration: Duration(milliseconds: 300),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildCreate() {
+    return CreateScreen(
+      category: widget.category,
+      onClose: _startTransitionAnimation,
     );
   }
 }
